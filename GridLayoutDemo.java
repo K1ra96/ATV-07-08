572eed7c3e157b5645e7234e6221c8e11db4f93b
K1ra96 <136936389+K1ra96@users.noreply.github.com>
2024-08-07 16:00:08 -0300

    Create GridLayoutDemo.java

---
+++ GridLayoutDemo.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class DoceDaVal{
+  public static void main( String[] args ){
+
+    GridLayoutFrame gridLayoutFrame = new GridLayoutFrame();
+    gridLayoutFrame. setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
+    gridLayoutFrame.setSize( 620, 480 );
+    gridLayoutFrame. setVisible( true );
+  } 
+}
