5a33495089fa9ecf5db3c7a3a19ecfb59e8e4bd7
K1ra96 <136936389+K1ra96@users.noreply.github.com>
2024-08-07 16:05:04 -0300

    Update DoceDaVal.java

--- DoceDaVal.java
+++ DoceDaVal.java
@@ -3,9 +3,9 @@ import javax.swing.JFrame;
 public class DoceDaVal{
   public static void main( String[] args ){
 
-    GridLayoutFrame gridLayoutFrame = new GridLayoutFrame();
-    gridLayoutFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
-    gridLayoutFrame.setSize( 620, 480 );
-    gridLayoutFrame.setVisible( true );
+    GridLayoutFrame1 gridLayoutFrame = new GridLayoutFrame();
+    gridLayoutFrame1.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
+    gridLayoutFrame1.setSize( 620, 480 );
+    gridLayoutFrame1.setVisible( true );
   } 
 }
