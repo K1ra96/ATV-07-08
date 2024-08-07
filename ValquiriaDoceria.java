78fcabf49250c51b20117f5c16e96952871ae300
K1ra96 <136936389+K1ra96@users.noreply.github.com>
2024-08-07 16:08:02 -0300

    Create ValquiriaDoceria.java

---
+++ ValquiriaDoceria.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class ValquiriaDoceria{
+  public static void main( String[] args ){
+
+    GridLayoutFrame2 gridLayoutFrame = new GridLayoutFrame();
+    gridLayoutFrame2.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
+    gridLayoutFrame2.setSize( 620, 480 );
+    gridLayoutFrame2.setVisible( true );
+  } 
+}
